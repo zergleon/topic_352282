dfdad058628fda62433558a48a45295b8b733b86
root <root@mail.lab.local>
2019-11-26 19:10:45 +0200

    change App.js-2

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './styles.css';
-
-const cars= [
-    {name: 'BMW M2 Coupe', price: '20000', img: 'https://mochamanstyle.com/wp-content/uploads/2015/10/2016-BMW-M2-Coupe.jpg' },
-    {name: 'Audi TT', price: '15000', img: 'https://article.images.consumerreports.org/w_598,h_436/prod/content/dam/cro/news_articles/cars/2016-Audi-TT-pr-1-2016-598' },
-    {name: 'Rolls Royce', price: '50000', img: 'http://cdn-ds.com/stock/2017-Bentley-Continental-GT-V8-Coupe--Beverly-Hills-CA/seo/VAMP16966-SCBFT7ZA0HC061335/sz_108215/image-1.jpg' },
-    {name: 'Mercedes amg coupe', price: '18000', img: 'https://auto.ndtvimg.com/car-images/big/mercedes-amg/gle-coupe/mercedes-amg-gle-coupe.jpg?v=2' }
-]
-
-function createCar(car) {
-    return `
-        <div class="card">
-            <div class="card-img">
-                <img
-                        src="${car.img}"
-                        alt="${car.name}"
-            </div>
-            <h3>${car.name}</h3>
-            <p>${car.price}</p>
-        </div>
-    `
-}
+import './styles.css'
 
 function App() {
   return (
-	const templates = cars.map(car => createCar(car))
-	const html = templates.join(' ')
-	
-	document.querySelector('.list').innerHTML = html
-);
+	<div className="card">
+            <div className="card-img">
+	    	<img 
+			src="https://mochamanstyle.com/wp-content/uploads/2015/10/2016-BMW-M2-Coupe.jpg"			
+			alt="BMW M2 Coupe">
+		</img>
+	    </div>	
+	    <h3>BMW M2 Coupe</h3>
+            <p>20000</p>
+        </div>	
+  );
 }
 
 export default App;
